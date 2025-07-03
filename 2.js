5e33594db49d9786eafb2689e31038f0c9877351
Pietro1-code <pietro.correa.silva@escola.pr.gov.br>
2025-07-03 16:43:46 -0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é um loop "for"?',
+    'O loop "for" é usado para iterar sobre uma sequência de elementos, como uma lista ou um intervalo de números.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em área?',
+    'O maior país do mundo em área é a Rússia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma estrutura condicional?',
+    'Uma estrutura condicional permite executar diferentes blocos de código dependendo de uma condição ser verdadeira ou falsa.'
+)
+
+criaCartao(
+    'Língua inglesa',
+    'Como se diz "bom dia" em inglês?',
+    '"Bom dia" em inglês é "Good morning" (gud môrning).'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa "debugging"?',
+    'Debugging é o processo de identificar e corrigir erros em um programa de computador.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Itália?',
+    'A capital da Itália é Roma.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma lista em Python?',
+    'Uma lista em Python é uma estrutura de dados que armazena uma coleção ordenada de itens.'
+)
+
+criaCartao(
+    'Língua inglesa',
+    'Como se diz "até logo" em inglês?',
+    '"Até logo" em inglês é "See you later" (síu lêiter).'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o deserto mais quente do mundo?',
+    'O deserto mais quente do mundo é o Deserto do Saara.'
+)
